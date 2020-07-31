dae2055d73c3ae3ba064dd7bb186b61c4887a3c8
Rene <renesegura@yahoo.com>
2020-07-31 12:09:34 -0700

    adding picture in picture, launching with button

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-console.log('testing');
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+
+// Prompt to select which media stream, pass to video element, then play
+async function selectMediaStream() {
+  try {
+    const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+    videoElement.srcObject = mediaStream;
+    videoElement.onloadedmetadata = () => {
+      videoElement.play();
+    }
+
+  } catch (error) {
+    // catch errors
+    console.log('whoops, error here: ', error);
+  }
+}
+button.addEventListener('click', async () => {
+  // Disable the button
+  button.disabled = true;
+  // Start Picture in Picture
+  await videoElement.requestPictureInPicture();
+  // Reset button, will only happen if await videoElement finishes
+  button.disabled = false;
+})
+
+// On Load
+selectMediaStream();
